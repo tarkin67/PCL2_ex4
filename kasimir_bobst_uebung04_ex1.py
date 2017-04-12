3c79509d381488443c13fb329c296477b4d72ae2
Kasimir Bobst <kingoftheroads96@gmail.com>
2017-04-12 13:16:07 +0200

    get all .xml filenames with glob

--- kasimir_bobst_uebung04_ex1.py
+++ kasimir_bobst_uebung04_ex1.py
@@ -4,3 +4,23 @@
 # Aufgabe 1
 # Autor: Kasimir Bobst
 # Matrikel-Nr.: 14 723 423
+
+import lxml.etree as ET
+import glob
+import sys
+
+
+def getfreqwords(indir, outfile):
+    for filename in glob.iglob(indir +
+                               "/SAC-Jahrbuch_[0-9][0-9][0-9][0-9]_mul.xml"):
+        print(filename)
+
+
+def main():
+    getfreqwords("C:/Users/kingo/Desktop/Text+Berg_Release_152_v01/" +
+                 "Text+Berg_Release_152_v01/Corpus_XML/SAC", "")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
