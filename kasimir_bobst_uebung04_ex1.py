f096f02a9ef86ccefb971fc3b270d3a8f5d9c8ae
Kasimir Bobst <kingoftheroads96@gmail.com>
2017-04-18 11:10:51 +0200

    write all sentences to a tmp file to save ram

--- kasimir_bobst_uebung04_ex1.py
+++ kasimir_bobst_uebung04_ex1.py
@@ -8,11 +8,13 @@
 import lxml.etree as ET
 import glob
 import sys
+import os
 
 
 def getfreqwords(indir, outfile):
     output_file = open(outfile, 'w', encoding="utf8")
-    sentence_hashes = {}
+    tmp_file_name = "tmp.txt"
+    temp_file = open(tmp_file_name, "w+", encoding="utf8")
     all_sentences = {}
     for filename in glob.iglob(indir +
                                "/SAC-Jahrbuch_[0-9][0-9][0-9][0-9]_mul.xml"):
@@ -23,13 +25,13 @@ def getfreqwords(indir, outfile):
                 sentence = ""
                 for element in node[1]:
                     if(element.tag == "w"):
-                        # print(str(element.get("lemma")))
                         sentence += str(element.get("lemma")) + " "
                 sentence = sentence[:len(sentence) - 1]
 
                 sentence_hash = hash(sentence)
 
-                sentence_hashes[sentence_hash] = sentence
+                save_hash_sentence_pair_in_file(
+                    sentence_hash, sentence, temp_file)
 
                 if(sentence_hash in all_sentences):
                     all_sentences[sentence_hash] += 1
@@ -38,13 +40,36 @@ def getfreqwords(indir, outfile):
 
             node[1].clear()
 
-        counter = 0
-        for val in sorted(all_sentences.items(),
-                          key=lambda x: x[1], reverse=True)[:20]:
-            print(sentence_hashes[val[0]] + ", " + str(val[1]))
-            counter += 1
+    temp_file.close()
+    print("Getting {} most common sentences...".format(str(20)))
+    for val in sorted(all_sentences.items(),
+                      key=lambda x: x[1], reverse=True)[:20]:
+
+        output_file.write("{}, {}\n".format(
+            read_sentence_from_file_with_hash(val[0], tmp_file_name),
+            str(val[1])))
 
     output_file.close()
+    print("Deleting temporary file...")
+    os.remove(tmp_file_name)
+    print("Done.")
+
+
+def save_hash_sentence_pair_in_file(hash_val, sentence, file):
+    """writes a sentence - hash pair to a temporary file"""
+    file.write("{}\t{}\n".format(str(hash_val), sentence))
+
+
+def read_sentence_from_file_with_hash(hash_val, file):
+    """reads a sentence from a temporary file with a key"""
+    temp_file = open(file, "r")
+    for line in temp_file:
+        # print("looking for {} in {}".format(str(hash_val), line))
+        if (line.startswith(str(hash_val))):
+            # return the substring starting with a tab
+            return line[line.find("\t") + 1:len(line) - 1]
+
+    temp_file.close()
 
 
 def main():
